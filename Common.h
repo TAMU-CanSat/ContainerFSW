04e90cd5ef70b716c590fef073616572f535ac37
WildWesley <86993555+WildWesley@users.noreply.github.com>
2022-05-08 22:38:27 -0500

    Update Common.h

--- Common.h
+++ Common.h
@@ -6,39 +6,46 @@
 #include <TeensyThreads.h>
 #include <ArduinoQueue.h>
 #include <TimeLib.h>
+#include <vector>
 
 #define GPS_SERIAL Serial1
 #define GROUND_XBEE_SERIAL Serial7
 #define PAYLOAD_XBEE_SERIAL Serial8
 
-namespace Common {
+namespace Common
+{
   static elapsedMillis milli;
-  const unsigned long TELEMETRY_DELAY = 1000; //1hz
-  
+  const unsigned long TELEMETRY_DELAY = 1000; // 1hz
+
   const byte VOLTAGE_PIN = 23;
   const byte BMP_SCL = 24;
   const byte BMP_SDA = 25;
   const byte PARA_SERVO_PIN = 41;
   const byte CAMERA_PIN = 3;
   const byte AUDIO_BEACON_PIN = 2;
-  
+
   const float SEA_LEVEL = 1014.6f;
-   
+
   static bool SIM_ACTIVATE = false;
   static bool SIM_ENABLE = false;
   static int SIM_PRESSURE = 0;
   const static uint16_t TEAM_ID = 1051;
   const static uint16_t LEAP_SECONDS = 18;
-  
+
   static uint16_t BA_ADDR = 0;
   static uint16_t PC_ADDR = 4;
   static uint16_t ST_ADDR = 6;
-  
+
   static float EE_BASE_ALTITUDE = 0;
   static uint16_t EE_PACKET_COUNT = 0;
 
   static String lastCMD = "None";
-  
+
+  // List to track acceleration measurements:
+  std::vector<std::float> altitudes[3];
+  std::vector<std::float> velocities[2];
+  float acceleration = 0f;
+
   struct GPS_Data
   {
     uint16_t hours;
@@ -50,7 +57,7 @@ namespace Common {
     float altitude;
     byte sats;
   };
-  
+
   struct Sensor_Data
   {
     float vbat;
@@ -60,13 +67,14 @@ namespace Common {
 
   static int millisecond()
   {
-    if (milli >= 1000) milli -= 1000;
+    if (milli >= 1000)
+      milli -= 1000;
     return milli;
   }
-  
-  static void build_packet(String& packet, const String& state, const char tp_released, const GPS_Data &gps, const Sensor_Data &sensors)
+
+  static void build_packet(String &packet, const String &state, const char tp_released, const GPS_Data &gps, const Sensor_Data &sensors)
   {
-    packet = TEAM_ID + ","; //0
+    packet = TEAM_ID + ","; // 0
     packet += String(hour()) + ":" + String(minute()) + ":" + String(second()) + "." + String(millisecond()) + ",";
     packet += String(EE_PACKET_COUNT) + ",";
     if (SIM_ACTIVATE && SIM_ENABLE)
@@ -74,13 +82,13 @@ namespace Common {
     else
       packet += "F,";
     packet += tp_released + ",";
-    packet += String(sensors.altitude) + ","; 
+    packet += String(sensors.altitude) + ",";
     packet += String(sensors.temperature) + ",";
     packet += String(sensors.vbat) + ",";
     packet += String(gps.hours) + ":" + String(gps.minutes) + ":" + String(gps.seconds) + "." + String(gps.milliseconds) + ",";
-    packet += String(gps.latitude) + ","; 
-    packet += String(gps.longitude) + ","; 
-    packet += String(gps.altitude) + ",";  
+    packet += String(gps.latitude) + ",";
+    packet += String(gps.longitude) + ",";
+    packet += String(gps.altitude) + ",";
     packet += String(gps.sats) + ",";
     packet += state + ",";
     packet += lastCMD + "\n";
