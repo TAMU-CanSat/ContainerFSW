3821929b4de2392693f8172102799900616e8313
WildWesley <86993555+WildWesley@users.noreply.github.com>
2022-05-13 14:47:31 -0500

    Simplify Landing.cpp

--- Landing.cpp
+++ Landing.cpp
@@ -3,9 +3,20 @@
 #include "States.h"
 
 namespace States
-{  
+{
   void Landing()
   {
-    // do landing
+    // Polling is stopped
+    // Poll_Payload.stop()
+    EEPROM.put(Common::ST_ADDR, 3);
+
+    // Container stops its telemetry
+    // Poll_Sensors.stop()
+
+    // Sound Beacon
+    Hardware::buzzer_on();
+
+    // Container stops recording
+    Hardware::update_camera(false);
   }
 }
