f63c08010cf6e2dea85f3682438109283118bf3d
WildWesley <86993555+WildWesley@users.noreply.github.com>
2022-05-17 15:31:13 -0500

    Compile-worthy Initialization.cpp

--- Initialization.cpp
+++ Initialization.cpp
@@ -1,26 +1,37 @@
 #include "Common.h"
 #include "Hardware.h"
 #include "States.h"
+#include <EEPROM.h>
 
 namespace States
 {  
-  uint16_t EE_STATE = 0;
-  
   void Initialization()
   {
     Common::GPS_Data gps_data;
     Common::Sensor_Data sensor_data;
-    
     Hardware::read_gps(gps_data);
     Hardware::read_sensors(sensor_data);
 
-    Hardware::start_recording();
-
     String packet;
-    Hardware::build_packet(packet, "INITIALIZATION", "N", gps_data, sensor_data); // build new packet
-    Serial.println(packet);
+    Common::build_packet(packet, "INITIALIZATION", 'N', gps_data, sensor_data);
   
+    Hardware::mtx.lock();
     Hardware::ground_packets.enqueue(packet);
-    //Hardware::payload_packets.enqueue("0");
+    Hardware::payload_packets.enqueue("0");
+    Hardware::mtx.unlock();
+
+    int item_count = Hardware::altitudes.itemCount();
+    if (item_count == 3)
+    {
+      float previous_altitude = Hardware::altitudes.dequeue();
+      float current_altitude = gps_data.altitude;
+      Hardware::altitudes.enqueue(current_altitude);
+    }
+
+    else if (item_count < 3)
+    {
+      float current_altitude = gps_data.altitude;
+      Hardware::altitudes.enqueue(current_altitude);
+    }
   }
 }
