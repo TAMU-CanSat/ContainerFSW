e0e1bedf4cf3d191d5c678ab47108f303c279ec6
WildWesley <86993555+WildWesley@users.noreply.github.com>
2022-05-17 15:27:31 -0500

    Compile-Worthy common.h

--- Common.h
+++ Common.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #ifndef __COMMON_H__
 #define __COMMON_H__
@@ -6,11 +7,10 @@
 #include <TeensyThreads.h>
 #include <ArduinoQueue.h>
 #include <TimeLib.h>
-#include <vector>
 
 #define GPS_SERIAL Serial1
-#define GROUND_XBEE_SERIAL Serial4
-#define PAYLOAD_XBEE_SERIAL Serial5
+#define  GROUND_XBEE_SERIAL Serial7
+#define PAYLOAD_XBEE_SERIAL Serial8
 
 namespace Common
 {
@@ -41,17 +41,11 @@ namespace Common
 
   static String lastCMD = "None";
 
-  // List to track acceleration measurements:
-  std::vector<std::float> altitudes[3];
-  std::vector<std::float> velocities[2];
-  float vertical_velocity = 0;
-  bool drogue_chute_deployed = false;
-
   struct GPS_Data
   {
-    uint8_t hours;
-    uint8_t minutes;
-    uint8_t seconds;
+    uint16_t hours;
+    uint16_t minutes;
+    uint16_t seconds;
     uint16_t milliseconds;
     float latitude;
     float longitude;
@@ -73,9 +67,9 @@ namespace Common
     return milli;
   }
 
-  static void build_packet(String &packet, const String &state, const char tp_released, const GPS_Data &gps, const Sensor_Data &sensors)
+  static void build_packet(String& packet, const String& state, const String& tp_released, const GPS_Data &gps, const Sensor_Data &sensors)
   {
-    packet = TEAM_ID + ","; // 0
+    packet = String(TEAM_ID) + ","; //0
     packet += String(hour()) + ":" + String(minute()) + ":" + String(second()) + "." + String(millisecond()) + ",";
     packet += String(EE_PACKET_COUNT) + ",";
     if (SIM_ACTIVATE && SIM_ENABLE)
@@ -83,16 +77,16 @@ namespace Common
     else
       packet += "F,";
     packet += tp_released + ",";
-    packet += String(sensors.altitude) + ",";
+    packet += String(sensors.altitude) + ","; 
     packet += String(sensors.temperature) + ",";
     packet += String(sensors.vbat) + ",";
     packet += String(gps.hours) + ":" + String(gps.minutes) + ":" + String(gps.seconds) + "." + String(gps.milliseconds) + ",";
-    packet += String(gps.latitude) + ",";
-    packet += String(gps.longitude) + ",";
-    packet += String(gps.altitude) + ",";
+    packet += String(gps.latitude) + ","; 
+    packet += String(gps.longitude) + ","; 
+    packet += String(gps.altitude) + ",";  
     packet += String(gps.sats) + ",";
     packet += state + ",";
-    packet += lastCMD + "\n";
+    packet += lastCMD;
   }
 }
 #endif
